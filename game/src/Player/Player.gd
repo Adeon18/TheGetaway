8770f09663435a1b0d6b3c7f12c0b68e66b7e0ac
Bohdan <bodya.ru02@gmail.com>
2021-12-11 12:22:29 +0200

    Add player movement

---
+++ game/src/Player/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var step = 8
+
+func _physics_process(delta):
+	var moveVector = Vector2()
+	if Input.is_action_just_pressed("ui_up"):
+		moveVector = Vector2.UP
+	elif Input.is_action_just_pressed("ui_down"):
+		moveVector = Vector2.DOWN
+	elif Input.is_action_just_pressed("ui_right"):
+		moveVector = Vector2.RIGHT
+	elif Input.is_action_just_pressed("ui_left"):
+		moveVector = Vector2.LEFT
+		
+	global_position += moveVector*step
