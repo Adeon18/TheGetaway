352c390b8bf7b3587357c9149b7222abd5b95471
Adeon18 <ostaptrush18@gmail.com>
2021-12-11 16:42:40 +0200

    Cleared up Pathfinding code

--- game/src/Enemy/Enemy.gd
+++ game/src/Enemy/Enemy.gd
@@ -5,29 +5,31 @@ var moveVector = Vector2()
 
 var cur_pos_tile
 var dest_pos_tile
+var next_pos_vec: Vector2
 
 var path: PoolVector2Array
 
 onready var Tilemap: TileMap = get_node("../../TileMap")
 onready var Player: Player = get_parent().get_node("Player")
 
+
 signal finish_turn
 
+
 func make_turn():
-#	print(used_cells)
-	
+	"""
+	Make a turn, get the current pos and the dest pos.
+	"""
 	cur_pos_tile = Tilemap.world_to_map(global_position)
 	dest_pos_tile = Tilemap.world_to_map(Player.global_position)
 	
 	path = Tilemap._get_path(cur_pos_tile, dest_pos_tile)
 	
-	
-	var next_pos = path[0] - cur_pos_tile
-	
-	print(path[0])
+	if path:
+		next_pos_vec = path[0] - cur_pos_tile
 	
 	moveVector = Vector2.RIGHT
-	global_position += next_pos * step
+	global_position += next_pos_vec * step
 	
 
 func get_class(): return "Enemy"
